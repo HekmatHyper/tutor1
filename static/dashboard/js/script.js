e7f3d6e409e6bca70ea00397ae7c695fdd593977
AliReZa <alireza.sarwarzade@yahoo.com>
2025-07-19 13:55:18 +0330

    complete dashboaard_base

---
+++ static/dashboard/js/script.js
@@ -0,0 +1,20 @@
+// Sidebar toggle
+function toggleSidebar() {
+    document.getElementById('sidebar').classList.toggle('collapsed');
+    document.getElementById('main-content').classList.toggle('full');
+}
+// Dark mode toggle
+function toggleDarkMode() {
+    var dark = document.body.classList.toggle('dark');
+    var icon = document.getElementById('toggleIcon');
+
+    const table = document.querySelector('table');
+    table.classList.toggle('table-dark', dark);
+
+    if (dark) {
+        icon.classList.replace('bi-moon-fill', 'bi-sun-fill');
+
+    } else {
+        icon.classList.replace('bi-sun-fill', 'bi-moon-fill');
+    }
+}
